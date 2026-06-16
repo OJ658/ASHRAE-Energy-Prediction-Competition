53cdcf4760a6b8012f0b63ceb175fb89205a72d7
oj_yassine <ojyassine348@gmail.com>
2026-06-16 12:42:51 +0100

    first commit of the second day

---
+++ notebooks/make_data.py
@@ -0,0 +1,13 @@
+### In this file, we will code functions that will be used for the preprocessing of the data
+
+
+import os
+import random
+import gc
+import tq
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
